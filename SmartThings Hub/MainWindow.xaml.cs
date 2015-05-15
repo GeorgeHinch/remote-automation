457979fd4c6133040e761af97bdab8e2cc6cb16e
George Hinchliffe <george@georgehinch.com>
2015-05-15 13:41:50 -0700

    Stack Panels instead of grids

--- SmartThings Hub/MainWindow.xaml.cs
+++ SmartThings Hub/MainWindow.xaml.cs
@@ -37,6 +37,26 @@ namespace SmartThings_Hub
             LockDate.Content = DateTime.Now.ToString("dddd, " + "MMMM dd" + ", " + "yyyy");
         }
     }
+
+    public class PercentageConverter : IValueConverter
+    {
+        public object Convert(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            return System.Convert.ToDouble(value) *
+                   System.Convert.ToDouble(parameter);
+        }
+
+        public object ConvertBack(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
 
 
