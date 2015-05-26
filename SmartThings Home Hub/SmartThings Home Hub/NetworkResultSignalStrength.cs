37025a43df4dd3728ddaa552a671f53d0174c311
George Hinchliffe <george@georgehinch.com>
2015-05-26 13:25:42 -0700

    Universal app updates

---
+++ SmartThings Home Hub/SmartThings Home Hub/NetworkResultSignalStrength.cs
@@ -0,0 +1,12 @@
+﻿namespace SmartThings_Hub
+{
+    public enum NetworkResultSignalStrength
+    {
+        ZERO,
+        ONE,
+        TWO,
+        THREE,
+        FOUR,
+        ETHER
+    }
+}
---
+++ SmartThings Home Hub/SmartThings Home Hub/NetworkStatusResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartThings_Hub
+{
+    public class NetworkStatusResult
+    {
+        String ssid;
+
+        NetworkResultSignalStrength sigStrength;
+
+        public string Ssid
+        {
+            get
+            {
+                return ssid;
+            }
+
+            set
+            {
+                ssid = value;
+            }
+        }
+
+        internal NetworkResultSignalStrength SigStrength
+        {
+            get
+            {
+                return sigStrength;
+            }
+
+            set
+            {
+                sigStrength = value;
+            }
+        }
+    }
+}
