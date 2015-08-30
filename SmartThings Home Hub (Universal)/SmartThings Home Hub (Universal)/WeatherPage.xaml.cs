c5418db6bd779a3647f369dc32d1444d889ca1ea
GeorgeHinch <George@georgehinch.com>
2015-08-30 03:18:23 -0700

    Fully working weather app

--- SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/WeatherPage.xaml.cs
+++ SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/WeatherPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -21,7 +22,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-using WUnderground.Client;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -39,7 +39,7 @@ namespace SmartThings_Home_Hub__Universal_
             string city = "Seattle";
             string country = "US";
 
-            WeatherLocation.Text = $"{city},{country}";
+            WeatherLocation.Text = $"{city}, {country}";
             HttpRequestMessage request = new HttpRequestMessage(
                 HttpMethod.Get,
                 $"http://api.openweathermap.org/data/2.5/weather?q={city},{country}");
@@ -51,28 +51,260 @@ namespace SmartThings_Home_Hub__Universal_
                 var bytes = Encoding.Unicode.GetBytes(result);
                 using (MemoryStream stream = new MemoryStream(bytes))
                 {
-                    var serializer = new DataContractJsonSerializer(typeof(Weather));
-                    var weather = (Weather)serializer.ReadObject(stream);
-                    WeatherTemp.Text = $"{(weather.main.temp - 273.15f) * 1.800 + 32:F0}°";
-                    WeatherLow.Text = $"« {(weather.main.temp_min - 273.15f) * 1.800 + 32:F0}°";
-                    WeatherHigh.Text = $"{(weather.main.temp_max - 273.15f) * 1.800 + 32:F0}° »";
-                    switch (weather.main.description)
+                    var serializer = new DataContractJsonSerializer(typeof(WeatherDetails));
+                    var weatherDetails = (WeatherDetails)serializer.ReadObject(stream);
+
+                    WeatherTemp.Text = $"{(weatherDetails.main.temp - 273.15f) * 1.800 + 32:F0}°";
+                    WeatherLow.Text = $"« {(weatherDetails.main.temp_min - 273.15f) * 1.800 + 32:F0}°";
+                    WeatherHigh.Text = $"{(weatherDetails.main.temp_max - 273.15f) * 1.800 + 32:F0}° »";
+                }
+
+                using (MemoryStream stream = new MemoryStream(bytes))
+                {
+                    var serializer = new DataContractJsonSerializer(typeof(WeatherStatus));
+                    var weatherStatus = (WeatherStatus)serializer.ReadObject(stream);
+
+                    switch (weatherStatus.weather[0].description)
                     {
+                        /// Thunderstorm
+                        case "thunderstorm with light rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "thunderstorm with rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "thunderstorm with heavy rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "light thunderstorm":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "thunderstorm":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "heavy thunderstorm":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "ragged thunderstorm":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "thunderstorm with light drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "thunderstorm with drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "thunderstorm with heavy drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+
+                        /// Drizzle
+                        case "light intensity drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "heavy intensity drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "light intensity drizzle rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "drizzle rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "heavy intensity drizzle rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "shower rain and drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "heavy shower rain and drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "shower drizzle":
+                            WeatherIcon.Text = $"";
+                            break;
+
+                        /// Rain
                         case "light rain":
-                            WeatherIcon.Text = $"&#61561;";
+                            WeatherIcon.Text = $"";
                             break;
-                        case "":
-                            WeatherIcon.Text = $"";
+                        case "moderate rain":
+                            WeatherIcon.Text = $"";
                             break;
-                        case " ":
-                            WeatherIcon.Text = $"";
+                        case "heavy intensity rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "very heavy rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "extreme rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "freezing rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "light intensity shower rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "shower rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "heavy intensity shower rain":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "ragged shower rain":
+                            WeatherIcon.Text = $"";
+                            break;
+
+                        /// Snow
+                        case "light snow":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "snow":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "heavy snow":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "sleet":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "shower sleet":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "light rain and snow":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "rain and snow":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "light shower snow":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "shower snow":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "heavy shower snow":
+                            WeatherIcon.Text = $"";
+                            break;
+
+                        /// Atmosphere
+                        case "mist":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "smoke":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "haze":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "sand, dust whirls":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "fog":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "sand":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "dust":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "volcanic ash":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "squalls":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "tornado":
+                            WeatherIcon.Text = $"";
+                            break;
+
+                        /// Clouds
+                        case "clear sky":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "few clouds":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "scattered clouds":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "broken clouds":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "overcast clouds":
+                            WeatherIcon.Text = $"";
+                            break;
+
+                        /// Extreme
+                        case "tropical storm":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "hurricane":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "cold":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "hot":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "windy":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "hail":
+                            WeatherIcon.Text = $"";
+                            break;
+
+                        /// Additional
+                        case "calm":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "light breeze":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "gentle breeze":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "moderate breeze":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "fresh breeze":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "strong breeze":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "high wind, near gale":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "gale":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "severe gale":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "storm":
+                            WeatherIcon.Text = $"";
+                            break;
+                        case "violent storm":
+                            WeatherIcon.Text = $"";
+                            break;
+
+                        /// Fallback
+                        default:
+                            WeatherIcon.Text = $"";
                             break;
                     }
                 }
             }
             else
             {
-                WeatherTemp.Text = "Err";
+                WeatherTemp.Text = "err";
             }
         }
 
@@ -86,13 +318,27 @@ namespace SmartThings_Home_Hub__Universal_
             public double temp { get; set; }
             public double temp_min { get; set; }
             public double temp_max { get; set; }
-            public string description { get; set; }
-    }
+        }
 
-        public class Weather
+        public class WeatherDetails
         {
             public Temperature main { get; set; }
         }
+
+        public class WeatherStatus
+        {
+            public int id { get; set; }
+            public String name { get; set; }
+            public int cod { get; set; }
+            public WeatherCondition[] weather { get; set; }
+        }
+
+        public class WeatherCondition
+        {
+            public int id { get; set; }
+            public String description { get; set; }
+            public String main { get; set; }
+            public String icon { get; set; }
+        }
     }
 }
-
