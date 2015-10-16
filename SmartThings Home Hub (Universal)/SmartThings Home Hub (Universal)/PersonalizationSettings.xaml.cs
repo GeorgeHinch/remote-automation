b5fb549c0ce57d19d55849da633076facebca5f8
GeorgeHinch <George@georgehinch.com>
2015-10-16 14:50:12 -0700

    Enable/Disable alert toggles based on main toggle

--- SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/PersonalizationSettings.xaml.cs
+++ SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/PersonalizationSettings.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -25,6 +26,82 @@ namespace SmartThings_Home_Hub__Universal_
         public PersonalizationSettings()
         {
             this.InitializeComponent();
+
+            var roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+
+            /* Load LED power toggle state */
+            if (roamingSettings.Values["alertStatus"] != null)
+            {
+                object alertValue = roamingSettings.Values["alertStatus"];
+                AlertModalsSwitch.IsOn = (bool)alertValue;
+            }
+            else
+            {
+                Debug.WriteLine("You fucked up.");
+                return;
+            }
+
+            alertOff_Toggler();
+        }
+
+        public void alertStatus_Toggled(object sender, RoutedEventArgs e)
+        {
+            var roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+
+            /* Store alert on/off status */
+            roamingSettings.Values["alertStatus"] = AlertModalsSwitch.IsOn;
+
+            alertOff_Toggler();
+        }
+
+        public void alertOff_Toggler()
+        {
+            var roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+
+            bool alertValue = (bool)roamingSettings.Values["alertStatus"];
+
+
+            if (alertValue == true)
+            {
+                alertsToggle_Enable();
+            }
+            else if (alertValue == false)
+            {
+                alertsToggle_Disable();
+            }
+        }
+
+        private void alertsToggle_Enable()
+        {
+            PulseLEDSwitch.IsEnabled = true;
+            SmartThingsLowBatterySwitch.IsEnabled = true;
+            SmartThingsKnockSwitch.IsEnabled = true;
+            SmartThingsEnviromentSwitch.IsEnabled = true;
+            SmartThingsAwayDoorSwitch.IsEnabled = true;
+            SeverWeatherSwitch.IsEnabled = true;
+            PowerStatusSwitch.IsEnabled = true;
+            NetworkStatusSwitch.IsEnabled = true;
+        }
+
+        private void alertsToggle_Disable()
+        {
+            PulseLEDSwitch.IsEnabled = false;
+            SmartThingsLowBatterySwitch.IsEnabled = false;
+            SmartThingsKnockSwitch.IsEnabled = false;
+            SmartThingsEnviromentSwitch.IsEnabled = false;
+            SmartThingsAwayDoorSwitch.IsEnabled = false;
+            SeverWeatherSwitch.IsEnabled = false;
+            PowerStatusSwitch.IsEnabled = false;
+            NetworkStatusSwitch.IsEnabled = false;
+        }
+              
+
+        private void PulseLEDSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            var roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+
+            /* Store LED Pulse on/off status */
+            roamingSettings.Values["ledPulseSwitch"] = PulseLEDSwitch.IsOn;
         }
     }
 }
