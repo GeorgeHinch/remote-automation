040f148334e8208934124d8489f8f47a477a8f7b
GeorgeHinch <George@georgehinch.com>
2015-10-31 11:21:53 -0700

    Moved power monitor into StatusBar.cs

--- SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/StatusBar.cs
+++ SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/StatusBar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.System.Power;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
@@ -18,6 +19,46 @@ namespace SmartThings_Home_Hub__Universal_
         public StatusBar()
         {
             this.DefaultStyleKey = typeof(StatusBar);
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            EventHandler<Object> stupd = new EventHandler<object>(this.timer_Tick);
+            timer.Tick += stupd;
+            timer.Start();
+        }
+
+        async void timer_Tick(object sender, object something)
+        {
+            //var wifiAdapters = await WiFiAdapter.FindAllAdaptersAsync();
+            //var firstWifiAdapter = wifiAdapters[0]; // be more careful, check size, etc...
+            //Debug.WriteLine(wifiAdapters[0]);       // again, check size, or look for your specific network
+            //var rssi = firstWifiAdapter.NetworkReport.AvailableNetworks[0].NetworkRssiInDecibelMilliwatts;
+            this.pwrIcon(this.getCurrentPowerSupplyStatus());
+            // do whatever you want with your RSSI
+
+        }
+
+        public PowerSupplyStatus getCurrentPowerSupplyStatus()
+        {
+            return Windows.System.Power.PowerManager.PowerSupplyStatus;
+        }
+
+        public void pwrIcon(PowerSupplyStatus status)
+        {
+            switch (status)
+            {
+                case PowerSupplyStatus.Inadequate:
+                    //strPowerLineStatus = "Device is running on battery";
+                    pwrStateIcon.Text = $"";
+                    break;
+                case PowerSupplyStatus.Adequate:
+                    //strPowerLineStatus = "Device is plugged in";
+                    pwrStateIcon.Text = $"";
+                    break;
+                case PowerSupplyStatus.NotPresent:
+                    //strPowerLineStatus = "Power Status: Unknown";
+                    pwrStateIcon.Text = $"";
+                    break;
+            }
         }
     }
 }
