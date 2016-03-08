1da7a26e816aa4ad1d92a27cc7e7ede2a1b3bb1d
GeorgeHinch <George@georgehinch.com>
2016-03-08 06:41:58 -0800

    Syled and completed news views

--- SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/NewsPage.xaml.cs
+++ SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/NewsPage.xaml.cs
@@ -32,10 +32,18 @@ namespace SmartThings_Home_Hub__Universal_
     /// </summary>
     public sealed partial class NewsPage : Page
     {
+        public static NewsPage newsPage;
+
         public NewsPage()
         {
             this.InitializeComponent();
+            newsPage = this;
+
             loadTopStories();
+
+            RadioButton item1 = (RadioButton)TopStories_Stackpanel.Children[0];
+            item1.IsChecked = true;
+            StoryWeb_View.Source = new Uri("http://www.nyt.com");
         }
 
         private void Home_Click(object sender, RoutedEventArgs e)
@@ -70,38 +78,12 @@ namespace SmartThings_Home_Hub__Universal_
                         var serializer = new DataContractJsonSerializer(typeof(NYT_TopStories));
                         NYT_TopStories stories = (NYT_TopStories)serializer.ReadObject(stream);
 
-                        Debug.WriteLine("Copyright: " + stories.copyright + " |");
-                        Debug.WriteLine("Section: " + stories.section + " |");
-
-                        //RadioButton[] radioButtons = new RadioButton[stories.num_results];
-
-                        //for (int i = 0; i < stories.num_results; ++i)
-                        //{
-                        //    radioButtons[i] = new RadioButton();
-                        //    radioButtons[i].Content = current.Text;
-                        //    radioButtons[i].Tag = current;
-                        //    radioButtons[i].GroupName = WebUtility.HtmlDecode(current.Icon);
-                        //    radioButtons[i].IsChecked = current.IsActive;
-                        //    radioButtons[i].Style = this.Resources["SplitViewNavButtonStyle"] as Style;
-                        //    radioButtons[i].Checked += new RoutedEventHandler(radioButton_Checked);
-
-                        //    this.TopStories_Stackpanel.Children.Add(radioButtons[i]);
-                        //}
-
                         foreach (NYT_Result st_re in stories.results)
                         {
-                            //Debug.WriteLine("Title: " + st_re.title + " |");
-                            //Debug.WriteLine("Abstract: " + st_re.story_abstract + " |");
-                            //Debug.WriteLine("-----");
-
-                            //foreach (Multimedia m in st_re.multimedia)
-                            //{
-                            //    Debug.WriteLine("Format: " + m.format + " |");
-                            //}
-
                             RadioButton story_rb = new RadioButton();
                             story_rb.Content = st_re.title;
-                            story_rb.Tag = st_re.story_abstract;
+                            story_rb.GroupName = st_re.story_abstract;
+                            story_rb.Tag = st_re;
                             story_rb.Style = this.Resources["NewsViewNavButtonStyle"] as Style;
                             story_rb.Checked += new RoutedEventHandler(radioButton_Checked);
 
@@ -115,7 +97,28 @@ namespace SmartThings_Home_Hub__Universal_
         private void radioButton_Checked(object sender, RoutedEventArgs routedEventArgs)
         {
             RadioButton radioButton = ((RadioButton)sender);
+            NYT_Result item = (NYT_Result)radioButton.Tag;
+
+            if(StoryWeb_Frame.Visibility != Visibility.Collapsed)
+            {
+                StoryWeb_Frame.Visibility = Visibility.Collapsed;
+            }
+
+            foreach (RadioButton rb in TopStories_Stackpanel.Children)
+            {
+                if(radioButton.Content != rb.Content)
+                {
+                    rb.IsChecked = false;
+                }
+            }
             radioButton.IsChecked = true;
+
+            StoryPreview_Frame.Navigate(typeof(NewsStoryPreview), item);
+        }
+
+        private void previewReturn_Clicked(object sender, RoutedEventArgs routedEventArgs)
+        {
+            StoryWeb_Frame.Visibility = Visibility.Collapsed;
         }
     }
 }
---
+++ SmartThings Home Hub (Universal)/SmartThings Home Hub (Universal)/NewsStoryPreview.xaml.cs
@@ -0,0 +1,72 @@
+﻿using SmartThings_Home_Hub__Universal_.Classes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SmartThings_Home_Hub__Universal_
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class NewsStoryPreview : Page
+    {
+        public string link;
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            NYT_Result result = e.Parameter as NYT_Result;
+            DateTime published = DateTime.Parse(result.updated_date, null, System.Globalization.DateTimeStyles.RoundtripKind);
+
+
+            IList<Multimedia> previewImage = result.multimedia;
+            foreach(Multimedia m in previewImage)
+            {
+                if(m.format == "superJumbo")
+                {
+                    Preview_Image.Source = new BitmapImage(new Uri(m.url, UriKind.Absolute));
+                }
+                else { Preview_Image.Source = null; }
+            }
+
+            Preview_Title.Text = result.title;
+            if(result.subsection == "")
+            {
+                Preview_Info.Text = result.section;
+            }
+            else { Preview_Info.Text = result.section + ", " + result.subsection; }
+            Preview_Byline.Text = result.byline;
+            Preview_Time.Text = "Published: " + published.ToString("M/d/yyyy, h:mm tt");
+            Preview_Abstract.Text = result.story_abstract;
+
+            link = result.url;
+
+            Debug.WriteLine("URL: " + result.url + " |");
+        }
+
+        public NewsStoryPreview()
+        {
+            this.InitializeComponent();
+        }
+
+        private void goToLink_Clicked(object sender, RoutedEventArgs e)
+        {
+            NewsPage.newsPage.StoryWeb_Frame.Visibility = Visibility.Visible;
+            NewsPage.newsPage.StoryWeb_View.Visibility = Visibility.Visible;
+            NewsPage.newsPage.StoryWeb_View.Source = new Uri(link);
+        }
+    }
+}
